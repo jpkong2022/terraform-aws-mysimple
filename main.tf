1e2fb2cb6e7cd78c999d4440b7d43e23514bac55
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2022-06-29 17:11:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-	region = "${var.region}"
-	access_key = "${var.access_key}"
-	secret_key = "${var.secret_key}"
+	region = var.region
+	access_key = var.access_key
+	secret_key = var.secret_key
 }
 
 
 resource "aws_instance" "test" {
-  ami = "${var.ami}"
-  instance_type = "${var.instance_type}"
+  ami = var.ami
+  instance_type = var.instance_type
   count = 1
   
 }
